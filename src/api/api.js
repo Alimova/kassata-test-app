005d62e13774ffc51be25f14bd6d1fe5acb13d43
Anna Alimova <new@alimov.net>
2019-11-18 18:08:04 +0200

    locations-autocomplete

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ const API_HOST = 'http://dataservice.accuweather.com/';
 const API_VERSION = 'v1';
 
 const weatherApiUrl = key => `${API_HOST}forecasts/${API_VERSION}/daily/1day/${key}?apikey=${API_KEY}`;
+const LocationApiUrl = (param, group, key) => `${API_HOST}locations/${API_VERSION}/${param}/${group}?apikey=${API_KEY}&q=${key}`;
 
 export function getWeather(key) {
   return fetch(weatherApiUrl(key))
@@ -10,6 +11,13 @@ export function getWeather(key) {
     .then(response => response.json())
     .catch(error => console.log(error));
 }
+
+export function getLocations(key) {
+    return fetch(LocationApiUrl('cities', 'search', key))
+    .then(handleErrors)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+  }
 
 function handleErrors(response) {
     if (!response.ok) {
--- src/components/weather/index.js
+++ src/components/weather/index.js
@@ -1,21 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { getWeather } from '../../api/api.js'; //TODO absolute path
+import React, { useState, useEffect, useRef } from 'react';
+import { getLocations, getWeather } from '../../api/api.js'; //TODO absolute path
 
 export default function Weather (props) {
+    const [searchQuery, searchCity] = useState('Tel Aviv');
+    const searchRef = useRef();
     const [currentCity] = useState('215854');
 
+    const predictions = getLocationsList(searchQuery);
+
+    async function getLocationsList(searchQuery){
+        console.log(searchQuery)
+        const response = await getLocations(searchQuery)
+        // const json = await response.json();
+        console.log(response.map(val => ({key: val.Key, name: val.EnglishName})))
+        // getWeather(json.Key);
+        return await response.map(val => ({key: val.Key, name: val.EnglishName})) || Array.prototype;
+    }
+
     useEffect(() => {
-        getWeather(currentCity)
-            .then((res) => {
-                console.log(res);
-            },
-            err => {
-                console.here('err ' + err);
-            },
-            ()=> {
-                console.log('error');
-            });
+        searchRef.current = searchQuery;
     });
 
-    return <div />
+    return (
+        <div className="box">
+            <div className="search-container">
+                <legend>Current city {currentCity}</legend>
+                <span className="icon"><i className="fa fa-search"></i></span>
+                <input
+                    type="search"
+                    value={searchQuery}
+                    onChange={e => searchCity(e.target.value)}
+                    id="search"
+                    placeholder="Search..."
+                />
+                <ul>
+                    {predictions.map((city) => (
+                    <li key={city.key}>{city.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+
+    )
 }
